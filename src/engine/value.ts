28643cbd696867fd2cecc4a4328bb1e15288f1d9
Raghunath Prabhakar <82239949+Raghuboi@users.noreply.github.com>
2025-09-16 14:10:46 -0500

    task: track operations

--- src/engine/value.ts
+++ src/engine/value.ts
@@ -1,13 +1,20 @@
+type Operation = "+" | "-" | "*" | "/" | undefined;
+
 class Value {
   data: number;
   _prev: Set<Value>;
+  _op: Operation;
 
   constructor(
     data: number,
-    { _children = [] }: { _children?: Array<Value> } = {}
+    {
+      _children = [],
+      _op = undefined,
+    }: { _children?: Array<Value>; _op?: Operation } = {}
   ) {
     this.data = data;
     this._prev = new Set(_children);
+    this._op = _op;
   }
 
   toString(): string {
@@ -20,15 +27,31 @@ class Value {
   }
 
   add(other: Value): Value {
-    return new Value(this.data + other.data, { _children: [this, other] });
+    return new Value(this.data + other.data, {
+      _children: [this, other],
+      _op: "+",
+    });
+  }
+
+  subtract(other: Value): Value {
+    return new Value(this.data - other.data, {
+      _children: [this, other],
+      _op: "-",
+    });
   }
 
   multiply(other: Value): Value {
-    return new Value(this.data * other.data, { _children: [this, other] });
+    return new Value(this.data * other.data, {
+      _children: [this, other],
+      _op: "*",
+    });
   }
 
   divide(other: Value): Value {
-    return new Value(this.data / other.data, { _children: [this, other] });
+    return new Value(this.data / other.data, {
+      _children: [this, other],
+      _op: "/",
+    });
   }
 }
 
